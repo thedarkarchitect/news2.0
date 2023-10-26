758c977cdfbf785b9d0e99f81a9d11b9ef75a2cf
thedarkarchitect <romanreynolds240@gmail.com>
2023-10-26 16:46:02 +0300

    final set up

--- app/src/main/java/com/example/newsapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/newsapp/ui/theme/Type.kt
@@ -2,9 +2,20 @@ package com.example.newsapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.newsapp.R
+
+val Poppins = FontFamily(
+    fonts = listOf(
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_bold, FontWeight.Bold),
+        Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    )
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,7 +25,37 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    displaySmall = TextStyle(
+        fontSize = 24.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 36.sp,
+    ),
+    displayMedium = TextStyle(
+        fontSize = 32.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 48.sp,
+    ),
+    bodySmall = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 21.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 24.sp,
+    ),
+    labelSmall = TextStyle(
+        fontSize = 13.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 19.sp,
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
