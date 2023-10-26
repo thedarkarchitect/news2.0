88301001dcbb5e4babcc852bcc5305cab30cc9ae
thedarkarchitect <romanreynolds240@gmail.com>
2023-10-27 00:17:02 +0300

    onboarding screen ready

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,19 +12,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.example.newsapp.presentation.onboarding.components.OnBoardingScreen
 import com.example.newsapp.ui.theme.NewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        installSplashScreen()
         setContent {
             NewsAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//
+//                }
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)){
+                    OnBoardingScreen()
                 }
             }
         }
---
+++ app/src/main/java/com/example/newsapp/presentation/onboarding/components/OnBoardingPage.kt
@@ -0,0 +1,70 @@
+package com.example.newsapp.presentation.onboarding.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.newsapp.presentation.Dimen.MediumPadding1
+import com.example.newsapp.presentation.Dimen.MediumPadding2
+import com.example.newsapp.presentation.onboarding.Page
+import com.example.newsapp.presentation.onboarding.pages
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+@Composable
+fun OnBoardingPage(
+    modifier: Modifier = Modifier,
+    page: Page
+){
+    Column(modifier = modifier) {
+        Image(
+           modifier = modifier
+               .fillMaxWidth()
+               .fillMaxHeight(fraction = 0.6f)
+            ,
+            painter = painterResource(id = page.image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = modifier.height(MediumPadding1))
+        Text(
+            text = page.title,
+            modifier = modifier.padding(horizontal = MediumPadding2),
+            style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.Bold),
+        )
+        Text(
+            text = page.title,
+            modifier = modifier.padding(horizontal = MediumPadding2),
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnBoardingPagePreview() {
+    NewsAppTheme {
+        OnBoardingPage(page = pages[0])
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Composable
+fun OnBoardingPage2Preview() {
+    NewsAppTheme {
+        OnBoardingPage(page = pages[0])
+    }
+}
